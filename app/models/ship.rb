a9e152d80a1a4ef59184e2d41dccb43912f2a9d0
Marius Rieder <marius.rieder@durchmesser.ch>
2015-06-07 14:32:47 +0200

    Add releation from ship to its possition

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,5 +1,8 @@
 class Ship < ActiveRecord::Base
 
+  # Relations
+  has_many :positions, class_name: "ShipPosition", foreign_key: 'mmsi', primary_key: 'mmsi'
+
   # Validations
   validates :name, presence: true
   validates :mmsi, numericality: true, presence: true
