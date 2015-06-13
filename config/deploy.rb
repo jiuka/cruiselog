866ef4d4ed800d5f12f954c8ed849639b380d676
Marius Rieder <marius.rieder@durchmesser.ch>
2015-06-13 11:49:40 +0200

    Restart after deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:application)}" }
 set :linked_files, fetch(:linked_files, []).push('.env')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -39,12 +39,12 @@ set :linked_files, fetch(:linked_files, []).push('.env')
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
+  after :restart, :restart do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :touch, 'tmp/restart.txt'
+      end
     end
   end
 
