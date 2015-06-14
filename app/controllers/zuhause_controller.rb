e371d21805c48e30f81b6d64563ac6733028212a
Marius Rieder <marius.rieder@durchmesser.ch>
2015-06-14 11:46:45 +0200

    Fix Spelling and position

--- app/controllers/zuhause_controller.rb
+++ app/controllers/zuhause_controller.rb
@@ -5,9 +5,9 @@ class ZuhauseController < ApplicationController
       format.geojson do
         entity_factory = ::RGeo::GeoJSON::EntityFactory.instance
         features = entity_factory.feature(
-          RGeo::Geographic.spherical_factory(srid: 4326).point(9.1520, 47.1165),
+          RGeo::Geographic.spherical_factory(srid: 4326).point(9.15134, 47.11601),
           'home', {icon: 'home', course: 0})
-        render geojson: features, bbox: RGeo::Geographic.spherical_factory(srid: 4326).point(9.1520, 47.1165)
+        render geojson: features, bbox: RGeo::Geographic.spherical_factory(srid: 4326).point(9.15134, 47.11601)
       end
     end
   end
