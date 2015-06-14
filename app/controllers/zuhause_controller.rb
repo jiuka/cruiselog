0b8272ec3a2624a7e3ff8236606f4e2eaa9d2042
Marius Rieder <marius.rieder@durchmesser.ch>
2015-06-14 12:57:29 +0200

    Fix typo

--- app/controllers/zuhause_controller.rb
+++ app/controllers/zuhause_controller.rb
@@ -4,7 +4,7 @@ class ZuhauseController < ApplicationController
       format.html
       format.geojson do
         point = RGeo::Geographic.spherical_factory(srid: 4326).point(9.15109, 47.11596)
-        entity_factory = ::RGeo::GeoJSON::EntityFactory.instancoe
+        entity_factory = ::RGeo::GeoJSON::EntityFactory.instance
         features = entity_factory.feature(point, 'home', {icon: 'home', course: 0})
         render geojson: features, bbox: point
       end
