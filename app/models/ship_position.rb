cf18cf438858e11c47c6836d80e121ac397af520
Marius Rieder <marius.rieder@durchmesser.ch>
2015-06-20 19:42:20 +0200

    Add ShipPosition validations

--- app/models/ship_position.rb
+++ app/models/ship_position.rb
@@ -1,4 +1,10 @@
+require 'date'
 class ShipPosition < ActiveRecord::Base
+
+  validates :timestamp, presence: true
+  validates :speed, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true
+  validates :course, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 360 }, allow_nil: true
+  validates :status, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 15 }, allow_nil: true
 
   def to_point
     factory = ::RGeo::Cartesian.preferred_factory()
--- spec/models/ship_position_spec.rb
+++ spec/models/ship_position_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe ShipPosition, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should have valid factory' do
+    expect(FactoryGirl.build(:ship_position)).to be_valid
+  end
+
+  context 'course' do
+    it 'should reject a negative course' do
+      expect(FactoryGirl.build(:ship_position, course: -1)).not_to be_valid
+    end
+
+    it 'should reject values bigger then 360' do
+      expect(FactoryGirl.build(:ship_position, course: 361)).not_to be_valid
+    end
+
+    it 'should validate 180 just fine' do
+      expect(FactoryGirl.build(:ship_position, course: 180)).to be_valid
+    end
+  end
+
+  context 'speed' do
+    it 'should reject a negative speed' do
+      expect(FactoryGirl.build(:ship_position, speed: -1)).not_to be_valid
+    end
+
+    it 'should reject a speed above 100' do
+      expect(FactoryGirl.build(:ship_position, speed: 101)).not_to be_valid
+    end
+
+    it 'validates a usual speed' do
+      expect(FactoryGirl.build(:ship_position, speed: 22.3)).to be_valid
+    end
+  end
+
+  context 'status' do
+    it 'should reject a negative status' do
+      expect(FactoryGirl.build(:ship_position, status: -1)).not_to be_valid
+    end
+
+    it 'should reject a status over 15' do
+      expect(FactoryGirl.build(:ship_position, status: 16)).not_to be_valid
+    end
+
+    it 'validates a status of 0' do
+      expect(FactoryGirl.build(:ship_position, status: 0)).to be_valid
+    end
+  end
+
+  context 'timestamp' do
+    it 'requires a timestamp' do
+      expect(FactoryGirl.build(:ship_position, timestamp: nil)).not_to be_valid
+    end
+
+    it 'rejects invalid dates' do
+      expect(FactoryGirl.build(:ship_position, timestamp: "2012-13-32T21:15:00")).not_to be_valid
+    end
+
+    it 'validates dates' do
+      expect(FactoryGirl.build(:ship_position, timestamp: "2015-06-20T16:13:59")).to be_valid
+    end
+  end
+
 end
